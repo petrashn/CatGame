2261019e509f0c007c72462e797fef9239a24a02
Klodin <klodin@hotmail.com>
2016-10-04 00:06:37 -0400

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
+    private Rigidbody2D rb2d;
+    private int coinCount = 0;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, verticalSpeed);
+        rb2d.AddForce(movement * horizontalSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            coinCount++;
+        }
+
+    }
+}
